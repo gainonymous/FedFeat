75a1485fbf0f9c6513b5e210074727977731f083
gainonymous <gain@khu.ac.kr>
2025-08-01 17:16:50 +0900

    Add files via upload

--- output/graph.py
+++ output/graph.py
@@ -1,1 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+method = "CNN"
+num_clients = 10
+
+cnn = np.load(f'cifar100_acc_cnn_FedAvg_FedFeat_noniid_10.npy')
+resnet = np.load(f'cifar100_acc_resnet_FedAvg_FedFeat_noniid_10.npy')
+
+print(f"CNN {max(cnn)}")
+print(f"ResNet {max(resnet)}")
